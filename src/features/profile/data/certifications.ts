b6cd84f28609a19bb103ab6865c680329dfa0187
ish-codes-magic <dipayanpal1729@gmail.com>
2025-07-15 09:56:21 +0530

    update: updated certificates

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -7,7 +7,7 @@ export const CERTIFICATIONS: Certification[] = [
     issuerLogoURL:
       "https://assets.ishita-codes-ai.in/images/companies/verix.webp",
     issueDate: "2025-05-14",
-    credentialID: "seo-47463-1745634751873",
+    credentialID: "42e1b25a-70be-48a2-9816-95e4f8602d69",
     credentialURL:
       "https://www.verix.io/credential/42e1b25a-70be-48a2-9816-95e4f8602d69",
   },
@@ -15,132 +15,41 @@ export const CERTIFICATIONS: Certification[] = [
     title: "Neural Networks and Deep Learning",
     issuer: "Coursera",
     issuerLogoURL:
-      "https://assets.ishita-codes-ai.in/images/companies/coursera.webp",
+      "https://assets.ishita-codes-ai.in/images/companies/coursera.png",
     issueDate: "2021-04-03",
-    credentialID: "543682",
+    credentialID: "39QKKK99KA6E",
     credentialURL:
       "https://www.coursera.org/account/accomplishments/verify/39QKKK99KA6E?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse",
   },
   {
-    title: "Next.js App Router Fundamentals",
-    issuer: "Vercel",
-    issuerIconName: "vercel",
-    issueDate: "2025-04-26",
-    credentialID: "dashboard-app-47463-1745633258744",
-    credentialURL:
-      "https://nextjs.org/learn/certificate?course=dashboard-app&user=47463&certId=dashboard-app-47463-1745633258744",
-  },
-  {
-    title: "React Foundations for Next.js",
-    issuer: "Vercel",
-    issuerIconName: "vercel",
-    issueDate: "2025-04-26",
-    credentialID: "react-foundations-47463-1745634245158",
-    credentialURL:
-      "https://nextjs.org/learn/certificate?course=react-foundations&user=47463&certId=react-foundations-47463-1745634245158",
-  },
-  {
-    title: "Certificate of Copyright Registration No. 0040/2025/QTG",
-    issuer: "Copyright Office of Viet Nam",
-    issuerLogoURL: "https://assets.chanhdai.com/images/companies/cov.webp",
-    issueDate: "2025-01-02",
-    credentialID: "0040/2025/QTG",
-    credentialURL:
-      "https://drive.google.com/file/d/1kVBByVLlYPyyUJvxKga670wvVPNmZ2nV/view?usp=sharing",
-  },
-  {
-    title: "Vietnamese Standardized Test of English Proficiency (CEFR B1)",
-    issuer: "USSH-VNUHCM",
+    title:
+      "Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning",
+    issuer: "Coursera",
     issuerLogoURL:
-      "https://assets.chanhdai.com/images/companies/ussh-vnuhcm.webp",
-    issueDate: "2024-12-17",
-    credentialID: "QH58202305187",
+      "https://assets.ishita-codes-ai.in/images/companies/coursera.png",
+    issueDate: "2021-03-31",
+    credentialID: "9M4CTT5UDQF6",
     credentialURL:
-      "https://vbcc.hcmussh.edu.vn/?type=11&certificateType=7&no=QH58202305187&fullName=Nguy%E1%BB%85n+Ch%C3%A1nh+%C4%90%E1%BA%A1i&dob=2000-08-14",
+      "https://www.coursera.org/account/accomplishments/verify/9M4CTT5UDQF6?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse",
   },
   {
-    title: "Certificate of Copyright Registration No. 7994/2024/QTG",
-    issuer: "Copyright Office of Viet Nam",
-    issuerLogoURL: "https://assets.chanhdai.com/images/companies/cov.webp",
-    issueDate: "2024-09-18",
-    credentialID: "7994/2024/QTG",
+    title: "Machine Learning",
+    issuer: "Coursera",
+    issuerLogoURL:
+      "https://assets.ishita-codes-ai.in/images/companies/coursera.png",
+    issueDate: "2020-10-25",
+    credentialID: "Z3UDZJJ37EL5",
     credentialURL:
-      "https://drive.google.com/file/d/1otjV4GNOLFj4JD2tHWLuZpUIVyITYNUy/view?usp=sharing",
+      "https://www.coursera.org/account/accomplishments/verify/Z3UDZJJ37EL5?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse",
   },
   {
-    title: "Engaging in Persuasive and Credible Communication",
-    issuer: "National University of Singapore",
-    issuerIconName: "coursera",
-    issueDate: "2022-09-13",
-    credentialID: "C6DHB9A5XQHV",
-    credentialURL: "https://coursera.org/verify/C6DHB9A5XQHV",
-  },
-  {
-    title: "Introduction to Databases",
-    issuer: "Meta",
-    issuerIconName: "meta",
-    issueDate: "2022-08-28",
-    credentialID: "YV5VQ5MXZ5YH",
-    credentialURL: "https://coursera.org/verify/YV5VQ5MXZ5YH",
-  },
-  {
-    title: "Solving Problems with Creative and Critical Thinking",
-    issuer: "IBM Skills Network",
-    issuerIconName: "coursera",
-    issueDate: "2022-08-27",
-    credentialID: "4UKZQJGM2932",
-    credentialURL: "https://coursera.org/verify/4UKZQJGM2932",
-  },
-  {
-    title: "Digital Skills: Social Media",
-    issuer: "Accenture",
-    issuerIconName: "accenture",
-    issueDate: "2022-08-27",
-    credentialID: "pc4345i",
-    credentialURL: "https://www.futurelearn.com/certificates/pc4345i",
-  },
-  {
-    title: "The Fundamentals of Digital Marketing",
-    issuer: "Google Digital Garage",
-    issuerIconName: "google",
-    issueDate: "2022-08-22",
-    credentialID: "3T8-F9N-LKQ",
+    title: "Programming for Everybody (Getting Started with Python)",
+    issuer: "Coursera",
+    issuerLogoURL:
+      "https://assets.ishita-codes-ai.in/images/companies/coursera.png",
+    issueDate: "2020-10-23",
+    credentialID: "LCWYADU3LA45",
     credentialURL:
-      "https://drive.google.com/file/d/14laAMWmVKhCfgKwVEgJyWse7o0sZDD4N/view?usp=sharing",
-  },
-  {
-    title: "Microsoft Office Specialist: Microsoft Office PowerPoint® 2013",
-    issuer: "Microsoft",
-    issuerIconName: "microsoft",
-    issueDate: "2017-04-16",
-    credentialID: "wXTm3-4SHG",
-    credentialURL:
-      "https://drive.google.com/file/d/117DE6bMkHvRqXbED2tmSMNkbx9s7OahM/view?usp=sharing",
-  },
-  {
-    title: "Google Code-in 2016",
-    issuer: "Google",
-    issuerIconName: "google",
-    issueDate: "2017-01-16",
-    credentialID: "",
-    credentialURL:
-      "https://drive.google.com/file/d/162RXtAVIZEvfx6LvP3xeBj-cSI9ZpPUX/view?usp=sharing",
-  },
-  {
-    title: "HSGS Olympiad 2016",
-    issuer: "HUS High School for Gifted Students",
-    issueDate: "2016-05-08",
-    credentialID: "",
-    credentialURL:
-      "https://drive.google.com/file/d/1eKv-2ldfw8-hF27sKfKWXL8MmwcSkzPq/view?usp=sharing",
-  },
-  {
-    title: "Microsoft Office Specialist: Microsoft Office Word 2013",
-    issuer: "Microsoft",
-    issuerIconName: "microsoft",
-    issueDate: "2016-04-24",
-    credentialID: "w9YXr-FMR8",
-    credentialURL:
-      "https://drive.google.com/file/d/1-NHhjKlQbhlcO7bpRue1XzDgDaudOf2N/view?usp=sharing",
+      "https://www.coursera.org/account/accomplishments/verify/LCWYADU3LA45?utm_source%3Dandroid%26utm_medium%3Dcertificate%26utm_content%3Dcert_image%26utm_campaign%3Dsharing_cta%26utm_product%3Dcourse",
   },
 ];
